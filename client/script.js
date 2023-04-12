4b9275023d2883ffd4f3c08b10de393b292116fc
Shahin Rizwana <95588187+Shahin2003@users.noreply.github.com>
2023-04-13 02:52:48 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -82,7 +82,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv);
 
-    const response = await fetch('https://openai-ahnv.onrender.com', {
+    const response = await fetch('https://codex-cxt3.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
